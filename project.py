c253e76761aeb5062b635a4a4b5981a27dd37985
Muhammed Salih <salih.four@gmail.com>
2021-02-27 10:55:18 +0530

    Make spacer module

--- project.py
+++ project.py
@@ -1,85 +1,93 @@
 import numpy as np
 from PIL import Image
 from numpy import asarray
+
+
+class spacer:
+    def __init__(self, spacelength):
+        self.lim = spacelength
+
+    def erase(self, image):
+        assert image.size[0] * image.size[1] > self.lim
+        data = image.load()
+        k = 0
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                chk = data[i,j] & 1
+                if chk == 1:
+                    data[i,j] = data[i,j] - 1
+                k = k + 1
+                if k == self.lim:
+                    break
+            if k == self.lim:
+                break
+
+    def insert(self, has, image):
+        assert image.size[0] * image.size[1] > self.lim
+        data = image.load()
+        k = 0
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                data[i,j] = data[i,j] + int(has[k])
+                k = k + 1
+                if k == self.lim:
+                    break
+            if k == self.lim:
+                break
+
+    def extract(self, image):
+        assert image.size[0] * image.size[1] > self.lim
+        data = image.load()
+        k = 0
+        chash = []
+        for i in range(image.size[0]):
+            for j in range(image.size[1]):
+                chk = data[i,j] & 1
+                if chk == 1:
+                    data[i,j] = data[i,j] - 1
+                    chash.append(1)
+                else:
+                    chash.append(0)
+                k = k + 1
+                if k == self.lim:
+                    break
+            if k == self.lim:
+                break
+        
+        return chash
+
+
+if __name__ == "__main__":
 # load the image
-image = Image.open('kolala.jpg').convert('L')
+    image = Image.open('veggies.jpg').convert('L')
+    hlen = 128
 
-# convert image to numpy array
-image.show()
+    # convert image to numpy array
+    image.save("veggies-before.jpg", "JPEG")
 
-data = asarray(image)
-print(type(data))
-# summarize shape
-print(data.shape)
-width, height = data.shape
-print(width, height)
-data = data.copy()
-print(data)
-lim = (image.size[0]*image.size[1])
+    sp = spacer(hlen)
 
+    data = asarray(image)
+    print(type(data))
+    # summarize shape
+    print(data.shape)
+    width, height = data.shape
+    print(width, height)
+    data = data.copy()
+    print(data)
 
-def erase(image):
-    data = image.load()
-    k = 0
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
-            chk = data[i,j] & 1
-            if chk == 1:
-                data[i,j] = data[i,j] - 1
-            k = k + 1
-            if k == lim:
-                break
-        if k == lim:
-            break
+    sp.erase(image)
+    data = asarray(image)
+    print(data)
+    image.save("veggies-erased.jpg", "JPEG")
 
-erase(image)
-data = asarray(image)
-print(data)
-# image.show()
+    has = np.random.randint(0,high = 2, size = hlen)
+    print(has)
+    sp.insert(has,image)
+    data = asarray(image)
+    print(data)
+    image.save("veggies-after.jpg", "JPEG")
 
-def insert(has, image):
-    data = image.load()
-    k = 0
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
-            data[i,j] = data[i,j] + int(has[k])
-            k = k + 1
-            if k == lim:
-                break
-        if k == lim:
-            break
+    chas = sp.extract(image)
 
-has = np.random.randint(0,high = 2, size = lim)
-print(has)
-insert(has,image)
-data = asarray(image)
-print(data)
-image.show()
-
-def extract(image):
-
-    data = image.load()
-
-    k = 0
-
-    chash = []
-
-    for i in range(image.size[0]):
-        for j in range(image.size[1]):
-            chk = data[i,j] & 1
-            if chk == 1:
-                data[i,j] = data[i,j] - 1
-                chash.append(1)
-            else:
-                chash.append(0)
-            k = k + 1
-            if k == lim:
-                break
-        if k == lim:
-            break
-    
-    return chash
-
-chas = extract(image)
-
-print((np.asarray(chas) == has).all())
+    print((np.asarray(chas) == has).all())
